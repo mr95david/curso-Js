2b44f111c6afbfdf91afc640fd8172b208e826be
mr95david <eliotrianar95@gmail.com>
2021-10-18 17:21:45 -0500

    Avances de la tarea de figuras del curso de platzi

--- figuras.js
+++ figuras.js
@@ -27,8 +27,9 @@ console.log(`La base del triángulo mide ${baseTriangulo}cm,
  */
 
 function perimetroTriangulo(lado1,lado2,base){
-    return lado1+lado2+base;
+    return lado1*1+lado2*1+base*1;
 };
+
 
 function areaTriangulo(alturaTriangulo,baseTriangulo){
     return alturaTriangulo*baseTriangulo/2;
@@ -65,7 +66,78 @@ const areaCirculo = radioCirculo*PI*PI;
 console.log(`El area del circulo es ${areaCirculo}cm^2`);
  */
 function areaCirculo(radioCirculo){
-    return radioCirculo*PI*PI;
+    return radioCirculo*PI*radioCirculo;
 }
 
 console.groupEnd();
+
+
+// Desde aqui interactuamos con HTML
+
+function calcularPerimetroCuadrado(){
+    const input = document.getElementById("ladoCuadrado");
+    const value = input.value;
+    let tipoOperacion = "perimetro";
+    let figura = "cuadrado";
+    const perimetro = perimetroCuadrado(value);
+    document.getElementById("typeOperation").innerHTML=tipoOperacion;
+    document.getElementById("fig").innerHTML=figura;
+    document.getElementById("resultado").innerHTML=perimetro;
+} ;
+
+function calcularAreaCuadrado(){
+    let input = document.getElementById("ladoCuadrado");
+    let value = input.value;
+    let tipoOperacion = "Area";
+    let figura = "Cuadrado";
+    let area = areaCuadrada(value);
+    document.getElementById("typeOperation").innerHTML=tipoOperacion;
+    document.getElementById("fig").innerHTML=figura;
+    document.getElementById("resultado").innerHTML=area;
+};
+
+function calcularPerimetroTriangulo(){
+    let lado1 = document.getElementById("lado1Triangulo");
+    let lado2 = document.getElementById("lado2Triangulo");
+    let base = document.getElementById("base");
+    let perimetro = perimetroTriangulo(lado1.value,lado2.value,base.value);
+    let tipoOperacion = "Perimetro";
+    let figura = "Triangulo";
+    document.getElementById("typeOperation").innerHTML=tipoOperacion;
+    document.getElementById("fig").innerHTML=figura;
+    document.getElementById("resultado").innerHTML=perimetro;
+};
+
+function calcularAreaTriangulo(){
+    let altura = prompt("Ingrese el valor de la altura del triangulo: ");
+    let base = document.getElementById("base");
+    let area = areaTriangulo(altura*1, base.value*1);
+    let tipoOperacion = "Area";
+    let figura = "Triangulo";
+    document.getElementById("typeOperation").innerHTML=tipoOperacion;
+    document.getElementById("fig").innerHTML=figura;
+    document.getElementById("resultado").innerHTML=area;
+};
+
+function calcularPerimetroCirculo(){
+    let radio = document.getElementById("radioCircle");
+    let valor = radio.value*1;
+    let diametro = diametroCirculo(valor);
+    let perimetro = perimetroCirculo(diametro);
+    let tipoOperacion = "Perimetro";
+    let figura = "Circulo";
+    document.getElementById("typeOperation").innerHTML=tipoOperacion;
+    document.getElementById("fig").innerHTML=figura;
+    document.getElementById("resultado").innerHTML=perimetro;
+};
+
+function calcularAreaCirculo(){
+    let radio = document.getElementById("radioCircle");
+    let valor = radio.value*1;
+    let area = areaCirculo(valor);
+    let tipoOperacion = "Area";
+    let figura = "Circulo";
+    document.getElementById("typeOperation").innerHTML=tipoOperacion;
+    document.getElementById("fig").innerHTML=figura;
+    document.getElementById("resultado").innerHTML=area;
+};
